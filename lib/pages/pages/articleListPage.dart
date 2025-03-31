960c1ee0d0aded0cafbb20c0b02c7a5992c5d183
F3rnand0hr <fernando.hidalgo@upr.edu>
2025-03-31 15:39:01 -0400

    refactored articles list page

--- lib/pages/pages/articleListPage.dart
+++ lib/pages/pages/articleListPage.dart
@@ -1,9 +1,19 @@
+import 'package:derecho_en_perspectiva/repositories/articles/articlesRepository.dart';
+import 'package:derecho_en_perspectiva/repositories/comments/commentsRepo.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:go_router/go_router.dart';
 
-class ArticleListPage extends StatelessWidget {
+class ArticleListPage extends StatefulWidget {
   const ArticleListPage({Key? key}) : super(key: key);
+
+  @override
+  State<ArticleListPage> createState() => _ArticleListPageState();
+}
+
+class _ArticleListPageState extends State<ArticleListPage> {
+  final _articleRepository = ArticleRepository();
+  final _commentsRepository = CommentsRepository();
 
   @override
   Widget build(BuildContext context) {
@@ -11,56 +21,7 @@ class ArticleListPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Articles'),
       ),
-      body: StreamBuilder<QuerySnapshot>(
-        stream: FirebaseFirestore.instance.collection('articulos').snapshots(),
-        builder: (context, snapshot) {
-          if (!snapshot.hasData) {
-            return const Center(child: CircularProgressIndicator());
-          }
-
-          final articles = snapshot.data!.docs;
-
-          if (articles.isEmpty) {
-            return const Center(
-              child: Text('No articles found.'),
-            );
-          }
-
-          return ListView.builder(
-            itemCount: articles.length,
-            itemBuilder: (context, index) {
-              final doc = articles[index];
-              final data = doc.data() as Map<String, dynamic>;
-              final title = data['title'] ?? 'Untitled';
-              final description = data['description'] ?? '';
-              final imageURL = data['imageURL'] ?? '';
-
-              return Card(
-                margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
-                child: ListTile(
-                  leading: imageURL.isNotEmpty
-                      ? Image.network(
-                          imageURL,
-                          width: 60,
-                          height: 60,
-                          fit: BoxFit.cover,
-                        )
-                      : const Icon(Icons.article),
-                  title: Text(title, style: const TextStyle(fontWeight: FontWeight.bold)),
-                  subtitle: Text(
-                    description,
-                    maxLines: 2,
-                    overflow: TextOverflow.ellipsis,
-                  ),
-                  onTap: () {
-                    context.push('/article/${doc.id}');
-                  },
-                ),
-              );
-            },
-          );
-        },
-      ),
+      body: _articleRepository.getArticlesStream(),
     );
   }
 }
--- lib/repositories/articles/articlesRepository.dart
+++ lib/repositories/articles/articlesRepository.dart
@@ -1,6 +1,8 @@
 // lib/repositories/article_repository.dart
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:derecho_en_perspectiva/data/models/articleModel.dart';
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class ArticleRepository {
   final CollectionReference<Map<String, dynamic>> _articlesRef =
@@ -26,6 +28,59 @@ class ArticleRepository {
 
       return Article.fromJson(mergedData);
     });
+  }
+
+  StreamBuilder<QuerySnapshot> getArticlesStream() {
+    return StreamBuilder<QuerySnapshot>(
+      stream: FirebaseFirestore.instance.collection('articulos').snapshots(),
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) {
+            return const Center(child: CircularProgressIndicator());
+          }
+
+          final articles = snapshot.data!.docs;
+
+          if (articles.isEmpty) {
+            return const Center(
+              child: Text('No articles found.'),
+            );
+          }
+
+          return ListView.builder(
+            itemCount: articles.length,
+            itemBuilder: (context, index) {
+              final doc = articles[index];
+              final data = doc.data() as Map<String, dynamic>;
+              final title = data['title'] ?? 'Untitled';
+              final description = data['description'] ?? '';
+              final imageURL = data['imageURL'] ?? '';
+
+              return Card(
+                margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+                child: ListTile(
+                  leading: imageURL.isNotEmpty
+                      ? Image.network(
+                          imageURL,
+                          width: 60,
+                          height: 60,
+                          fit: BoxFit.cover,
+                        )
+                      : const Icon(Icons.article),
+                  title: Text(title, style: const TextStyle(fontWeight: FontWeight.bold)),
+                  subtitle: Text(
+                    description,
+                    maxLines: 2,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                  onTap: () {
+                    context.push('/article/${doc.id}');
+                  },
+                ),
+              );
+            },
+          );
+      },
+    );
   }
 
   // Like an article (add userId to 'liked' list)
